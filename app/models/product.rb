57ce018b61d6829adc05b5bdfca0aae1374e9b5d
joseph Borremans <joseph.borremans@gmail.com>
2012-10-20 15:42:49 +0200

    add to cart button

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   
   default_scope :order => 'title'
+  before_destroy :ensure_not_referenced_by_any_line_item 
+  has_many :line_items
+  
   
   attr_accessible :description, :image_url, :price, :title
   validates :title,  :description, :image_url, :presence => true
@@ -10,5 +13,17 @@ class Product < ActiveRecord::Base
     :with  => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PGN image'
   }
+  validates :title, length: {minimum: 10}
   
+  private
+  
+    # ensure that there are no line items referencing this product 
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:add, 'Line Items present')
+        return false
+      end
+    end  
 end
