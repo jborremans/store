0dd1d259447061a07477ebe0879e91f9aca5a046
joseph Borremans <joseph.borremans@gmail.com>
2012-11-05 23:01:23 +0100

    added links - persistent problems with laoding shopping cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,14 +4,21 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
     if current_item
-      current_item.quantity += 1
+      current_item.quantity + 1
     else
       current_item = line_items.build(:product_id => product_id)
     end
     current_item
   end
 
+  # def total_price
+  #       line_items.to_a.sum { |item| item.total_price }
+  #  end
+  
   def total_price
-    line_items.to_a.sum { |item| item.total_price }
+      line_items.to_a.each(&:total_price).sum
   end
 end
+
+  
+ 
