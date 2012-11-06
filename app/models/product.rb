1785a49081b02ccb122b02c4486fa15ab026b0fc
joseph Borremans <joseph.borremans@gmail.com>
2012-11-06 09:40:13 +0100

    reviews/still problems with cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,15 +6,10 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  #...
-
-
   # validation stuff...
-
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-# 
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with    => %r{\.(gif|jpg|png)$}i,
@@ -24,6 +19,7 @@ class Product < ActiveRecord::Base
   private
 
     # ensure that there are no line items referencing this product
+    
     def ensure_not_referenced_by_any_line_item
       if line_items.empty?
         return true
